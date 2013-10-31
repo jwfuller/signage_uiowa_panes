d5daa1c49512bd3be3ca67b6519e2d4e41e081e1
James Fuller <james-w-fuller@uiowa.edu>
2013-10-31 16:31:35 -0500

    correct js file

--- js/pentacrestcam.js
+++ js/pentacrestcam.js
@@ -3,23 +3,6 @@
  * Javascript for the signage panes module.
  */
 
-// var ImageReloaded = 0;
-// var BaseURL = "http://webcam.iowa.uiowa.edu/live/readImage.asp";
-// var theTimer = setTimeout('reloadImage()', 1);
-// function ImageLoaded() {
-//   ImageReloaded = 1;
-// }
-// function reloadImage() {
-//   theTimer = setTimeout('reloadImage()', 1000);
-//   if (ImageReloaded = 1) {
-//     theDate = new Date();
-//     var url = BaseURL;
-//     url += '?dummy=' + theDate.getTime().toString(10);
-//     document.webCamImage.src = url;
-//     ImageReloaded = 0;
-//   }
-// }
-
  // Namespace jQuery to avoid conflicts.
 (function($) {
   // Attach Pentacrest Cam behavior.
@@ -32,7 +15,6 @@
   };
   // Define the behavior.
   Drupal.pentacrestcam = function() {
-
 
     setInterval(function () {
       d = new Date();
